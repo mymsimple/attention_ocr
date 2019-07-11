308d85392e9422aec859482c7723efc53eee01c3
piginzoo <piginzoo@gmail.com>
2019-07-11 19:28:46 +0800

    update

--- layers/attention.py
+++ layers/attention.py
@@ -3,7 +3,7 @@ import os
 from tensorflow.python.keras.layers import Layer
 from tensorflow.python.keras import backend as K
 
-
+# 实现了经典的attention模式：https://arxiv.org/pdf/1409.0473.pdf
 class AttentionLayer(Layer):
     """
     This class implements Bahdanau attention (https://arxiv.org/pdf/1409.0473.pdf).
@@ -42,80 +42,65 @@ class AttentionLayer(Layer):
             print('encoder_out_seq>', encoder_out_seq.shape)
             print('decoder_out_seq>', decoder_out_seq.shape)
 
-        def energy_step(inputs, states):
-            """ Step function for computing energy for a single decoder state """
+        # 实现了能量函数，e_tj=V * tanh ( W * h_j + U * S_t-1 + b )
+        # inputs,我理解就是所有的h_j，错！我之前理解错了，这个参数是指某个时刻t，对应的输入！不是所有，是某个时刻的输入。
+        #        按为什么还有个s，input+s，是因为batch。
+        # states,我理解就是S_t-1
+        def energy_step(inputs, states): # inputs(batch,dim)
 
-            assert_msg = "States must be a list. However states {} is of type {}".format(states, type(states))
-            assert isinstance(states, list) or isinstance(states, tuple), assert_msg
-
-            """ Some parameters required for shaping tensors"""
             en_seq_len, en_hidden = encoder_out_seq.shape[1], encoder_out_seq.shape[2]
             de_hidden = inputs.shape[-1]
 
-            """ Computing S.Wa where S=[s0, s1, ..., si]"""
-            # <= batch_size*en_seq_len, latent_dim
+            #  W * h_j
             reshaped_enc_outputs = K.reshape(encoder_out_seq, (-1, en_hidden))
-            # <= batch_size*en_seq_len, latent_dim
             W_a_dot_s = K.reshape(K.dot(reshaped_enc_outputs, self.W_a), (-1, en_seq_len, en_hidden))
-            if verbose:
-                print('wa.s>',W_a_dot_s.shape)
 
-            """ Computing hj.Ua """
+            # U * S_t - 1
             U_a_dot_h = K.expand_dims(K.dot(inputs, self.U_a), 1)  # <= batch_size, 1, latent_dim
-            if verbose:
-                print('Ua.h>',U_a_dot_h.shape)
 
-            """ tanh(S.Wa + hj.Ua) """
-            # <= batch_size*en_seq_len, latent_dim
+            # tanh ( W * h_j + U * S_t-1 + b )
             reshaped_Ws_plus_Uh = K.tanh(K.reshape(W_a_dot_s + U_a_dot_h, (-1, en_hidden)))
-            if verbose:
-                print('Ws+Uh>', reshaped_Ws_plus_Uh.shape)
 
-            """ softmax(va.tanh(S.Wa + hj.Ua)) """
-            # <= batch_size, en_seq_len
+            # V * tanh ( W * h_j + U * S_t-1 + b )
             e_i = K.reshape(K.dot(reshaped_Ws_plus_Uh, self.V_a), (-1, en_seq_len))
-            # <= batch_size, en_seq_len
-            e_i = K.softmax(e_i)
 
-            if verbose:
-                print('ei>', e_i.shape)
+            # softmax(e_tj)
+            e_i = K.softmax(e_i)
 
             return e_i, [e_i]
 
-
-        # encoder_out_seq: 编码器的各个time sequence的输出h_i
-        # states: 解码器的状态
-        # inputs：解码器的输入，对于attention来说，就是和 alpha_i啊，我理解
-        def context_step(inputs, states):
-            """ Step function for computing ci using ei """
-            # <= batch_size, hidden_size
+        # 这个step函数有意思，特别要关注他的入参：
+        # encoder_out_seq: 编码器的各个time sequence的输出h_i，[batch,ts,dim]
+        # states:
+        # inputs：某个时刻，这个rnn的输入，这里，恰好是之前那个能量函数eij对应这个时刻的概率
+        # ----------------------------
+        # "step_do 这个函数，这个函数接受两个输入：step_in 和 states。
+        #   其中 step_in 是一个 (batch_size, input_dim) 的张量，
+        #   代表当前时刻的样本 xt，而 states 是一个 list，代表 yt−1 及一些中间变量。"
+        def context_step(inputs, states): # inputs (batch,dim)
             c_i = K.sum(encoder_out_seq * K.expand_dims(inputs, -1), axis=1)
-            if verbose:
-                print('ci>', c_i.shape)
             return c_i, [c_i]
 
         def create_inital_state(inputs, hidden_size):
             # We are not using initial states, but need to pass something to K.rnn funciton
-            fake_state = K.zeros_like(inputs)  # <= (batch_size, enc_seq_len, latent_dim
+            fake_state = K.zeros_like(inputs)  # <= (batch_size, enc_seq_len, latent_dim)
             fake_state = K.sum(fake_state, axis=[1, 2])  # <= (batch_size)
             fake_state = K.expand_dims(fake_state)  # <= (batch_size, 1)
-            fake_state = K.tile(fake_state, [1, hidden_size])  # <= (batch_size, latent_dim
+            fake_state = K.tile(fake_state, [1, hidden_size])  # <= (batch_size, latent_dim)
             return fake_state
 
         fake_state_c = create_inital_state(encoder_out_seq, encoder_out_seq.shape[-1])
         fake_state_e = create_inital_state(encoder_out_seq, encoder_out_seq.shape[1])  # <= (batch_size, enc_seq_len, latent_dim
 
-        """ Computing energy outputs """
-        # e_outputs => (batch_size, de_seq_len, en_seq_len)
-        # 计算每个e_i,这步是算e_i
+        # K.rnn(计算函数，输入x，初始状态）: K.rnn 这个函数，接受三个基本参数，其中第一个参数就是刚才写好的 step_do 函数，第二个参数则是输入的时间序列，第三个是初始态
+        # 这个rnn就是解码器，输入 eji=a(s_i-1,hj)，其中j要遍历一遍，这个k.rnn就是把每个hj对应的eij都计算一遍
+        # 输出e_outputs，就是一个概率序列
         last_out, e_outputs, _ = K.rnn(
             energy_step, decoder_out_seq, [fake_state_e],
         )
 
-        """ Computing context vectors """
-        # 得到编码器每步对应的e_i，就可以算alhpa_i，然后\Sum就可以求C_i(attenion向量)，然后带入到rnn中，算对应的解码了
-        # 这步是算c_i，C_t = Sum( Alpha_tj * Hj )
-        last_out, c_outputs, _ = K.rnn(
+        # eij(i不变,j是一个encoder的h下标），灌入到一个新的rnn中，让他计算出对应的输出，这个才是真正的Decoder！！！
+        last_out, c_outputs, _ = K.rnn( # K.rnn是构建了一个rnn网络，一步步的算，每步都调用
             context_step, e_outputs, [fake_state_c],
         )
 
